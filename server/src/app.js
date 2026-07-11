55e5f33703163b355701a5097b0b4454ab6bcacd
Fazeel Khan <fazeelk2004@gmail.com>
2026-07-11 18:00:58 +0500

    feat: prepared for development

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
+import path from "path"
 
 import apiLimiter from './middleware/rateLimiter.js';
 import notFound from './middleware/notFound.js';
@@ -8,17 +9,20 @@ import errorHandler from './middleware/errorHandler.js';
 import routes from './routes/index.js';
 
 const app = express();
+const __dirname = path.resolve();
 
 // 1. Security headers
 app.use(helmet());
 
 // 2. Cross-origin resource sharing.
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
-    credentials: true,
-  })
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: process.env.CLIENT_URL || 'http://localhost:5173',
+      credentials: true,
+    })
+  );
+}
 
 // 3. Body parsers
 app.use(express.json({ limit: '10kb' }));
@@ -29,6 +33,13 @@ app.use('/api', apiLimiter);
 
 // 5. Application routes.
 app.use('/api', routes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")))
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+  })
+}
 
 // 6. Unmatched routes -> 404.
 app.use(notFound);
